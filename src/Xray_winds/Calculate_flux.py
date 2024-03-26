22c7b17438771610fdf398ea80b1f71e3dfdd0e7
Tobias Eikelenboom <tobias@Tobiass-MacBook-Air.local>
2024-03-26 15:16:39 +0100

    Change in folder structure to fix the installation issue

---
+++ src/Xray_winds/Calculate_flux.py
@@ -0,0 +1,150 @@
+
+from email.mime import image
+from tkinter import Grid
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import Grid_Operations
+
+__all__ = ['find_nearest', 'simple_g', 'G', 'projection_2d', 'total_lum_wvl_bin',
+           'create_list_of_tuples', 'create_spectra']
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    lowest_idx = abs(array - value).argmin()
+    return lowest_idx
+
+def simple_g(T, *args, **kwargs):
+    return np.where((T >=1e6) * (T <= 1e7), 1, 0)
+
+def G(T, wavelength_band:tuple, which_g='', **kwargs):
+    from scipy.interpolate import interp1d
+    if which_g.lower() == 'simple':
+        return simple_g(T)
+    elif which_g.lower() == 'geom':
+        G_total = np.load('G_(T,L)/G-1e-05_geomwvl.npy')
+        wvl_array = np.load('G_(T,L)/geom-wvl.npy')
+        T_array = np.load('G_(T,L)/temps.npy')
+    else:
+        # Load in the created G function and the corresponding wvl,T grid
+        G_total = np.load('G_(T,L)/G-1e-05.npy')
+        wvl_array = np.load('G_(T,L)/wvl.npy')
+        T_array = np.load('G_(T,L)/temps.npy')
+        
+    # Find the indicies of the closest wavelengths on the grid
+    low_wvl_idx = find_nearest(wvl_array, wavelength_band[0])
+    high_wvl_idx = find_nearest(wvl_array, wavelength_band[1])
+    G_integrated_across_band = np.trapz(G_total[...,low_wvl_idx:high_wvl_idx],
+                                        wvl_array[...,low_wvl_idx:high_wvl_idx], axis=-1)
+    
+    # Interpolate G for certain wavelength
+    T_interpolator = interp1d(T_array, G_integrated_across_band)
+    interpolated_fluxes = T_interpolator(T)
+    return interpolated_fluxes
+
+
+def projection_2d(wvl_bin: tuple, stellar_radius: float, interpolator, var_list: list,
+                                  image_radius=20, pixel_count=100., angle=(0.,0.), grid_type='linear', *args, **kwargs) -> tuple:
+
+    image_radius *= stellar_radius
+    
+    if grid_type.lower() == 'segmented':
+        # We'll create a whole grid and mask out the middle, and create a seperate middle (Primed) grid
+        # which is the size of the masked out part with the same resolution
+        (X, Y ,Z), _ = Grid_Operations.create_grid(image_radius, pixel_count, type='linear')
+        (X_prime, Y_prime, Z_prime), _ = Grid_Operations.create_grid(image_radius/3, pixel_count, type='linear')
+        X_rot, Y_rot, Z_rot = Grid_Operations.rotate_grid(angle[0], angle[1], (X, Y ,Z))
+        Xprime_rot, Yprime_rot, Zprime_rot = Grid_Operations.rotate_grid(angle[0], angle[1], (X_prime, Y_prime ,Z_prime))
+
+        # The star is only present in the middel grid so we do not have to mask the star in the outer grid
+        mask_star = Xprime_rot ** 2 + Yprime_rot ** 2 + Zprime_rot ** 2 <= stellar_radius ** 2
+        mask_shadow = (Y_prime ** 2 + Z_prime ** 2 <= stellar_radius ** 2) * (X_prime < 0)
+        inner_mask = mask_star + mask_shadow
+        
+        inner_masked_in_outer =  (X < image_radius/3) * (Y < image_radius/3) * (Z < image_radius/3) \
+            * (-image_radius/3 < X) * (-image_radius/3 < Y) * (-image_radius/3 < Z)
+        interpolated_inner = interpolator(Xprime_rot, Yprime_rot, Zprime_rot)
+        interpolated_outer = interpolator(X_rot, Y_rot, Z_rot)
+
+        integrand_inner = np.square(interpolated_inner[...,var_list.index('Rho [g/cm^3]')] / 1.67e-24) * G(interpolated_inner[...,var_list.index('te [K]')], wvl_bin, *args, **kwargs)
+        integrand_outer = np.square(interpolated_outer[...,var_list.index('Rho [g/cm^3]')] / 1.67e-24) * G(interpolated_outer[...,var_list.index('te [K]')], wvl_bin, *args, **kwargs)
+        
+        masked_integrand_outer = np.where(inner_masked_in_outer==False, integrand_outer, 0)
+        masked_integrand_inner = np.where(inner_mask==False, integrand_inner, 0)
+
+        # We want to take into account that light that goes towards the star can't be accounted fors
+        Area_of_star = np.square(stellar_radius) * np.pi 
+        # Creating an array in the shape of the data to calculate the solid angle of the star at each point
+        solid_angle_array_outer = Area_of_star / (X_rot**2 + Y_rot**2 + Z_rot**2)
+        solid_angle_array_inner = Area_of_star / (Xprime_rot**2 + Yprime_rot**2 + Zprime_rot**2)
+
+        # The fraction of the sky taken up by the star at a point is then solid_angle / 4pi
+        # So the fraction light that escapes is 1 - solid_angle/4pi
+        fraction_usable_light_outer = 1 - (solid_angle_array_outer / (4*np.pi))
+        fraction_usable_light_inner = 1 - (solid_angle_array_inner / (4*np.pi))
+
+        masked_integrand_inner *= fraction_usable_light_inner
+        masked_integrand_outer *= fraction_usable_light_outer
+
+        flux_inner = np.trapz(masked_integrand_inner, X_prime, axis=1)
+        flux_outer = np.trapz(masked_integrand_outer, X, axis=1)
+        return (flux_inner, flux_outer), ((Y_prime[:, 0, :], Z_prime[:, 0, :]), (Y[:, 0, :], Z[:, 0, :]))
+    
+    else:
+        (X, Y ,Z), _ = Grid_Operations.create_grid(image_radius, pixel_count, type=grid_type)
+        X_rot, Y_rot, Z_rot = Grid_Operations.rotate_grid(angle[0], angle[1], (X, Y ,Z))
+        # Interpolate the data on that grid
+        interpolated_data = interpolator(X_rot, Y_rot, Z_rot)
+        # From the mesh grid create a mask that removes the star
+        mask_star = X_rot ** 2 + Y_rot ** 2 + Z_rot ** 2 <= stellar_radius ** 2
+        mask_shadow = (Y ** 2 + Z ** 2 <= stellar_radius ** 2) * (X < 0)
+        # Mask the shadow of the star
+        mask = mask_star + mask_shadow
+        integrand = np.square(interpolated_data[...,var_list.index('Rho [g/cm^3]')] / 1.67e-24) * G(interpolated_data[...,var_list.index('te [K]')], wvl_bin, *args, **kwargs)
+        masked_integrand = np.where(mask==False, integrand, 0)
+
+        # We want to take into account that light that goes towards the star can't be accounted fors
+        Area_of_star = np.square(stellar_radius) * np.pi 
+        # Creating an array in the shape of the data to calculate the solid angle of the star at each point
+        solid_angle_array = Area_of_star / (X_rot**2 + Y_rot**2 + Z_rot**2)
+
+        # The fraction of the sky taken up by the star at a point is then solid_angle / 4pi
+        # So the fraction light that escapes is 1 - solid_angle/4pi
+        fraction_usable_light =  1 - (solid_angle_array / (4*np.pi))
+        # print(solid_angle_array)
+        masked_integrand *= fraction_usable_light
+
+        # Now we integrate along the line of sight
+        total_flux = np.trapz(masked_integrand, X, axis=1)
+        return total_flux, (Y[:, 0, :], Z[:, 0, :])
+    
+def projection_to_total_lum(wvl_bin:tuple, stellar_radius, interpolator, *args, **kwargs) -> float:
+    LoS_flux, mesh = projection_2d(wvl_bin, stellar_radius, interpolator=interpolator, *args, **kwargs)
+    integrate_axis1 = np.trapz(LoS_flux, mesh[1], axis=-1)
+    # Then integrate along the other to get a single value
+    total_flux_in_band = np.trapz(integrate_axis1, mesh[1][0])
+    return total_flux_in_band
+
+def create_list_of_tuples(lst1:list|np.ndarray, lst2:list|np.ndarray) -> list:
+    result = []  # Empty list to store the tuples
+    for i in range(len(lst1)):
+        # Create a tuple from corresponding elements
+        tuple_element = (lst1[i], lst2[i])
+        result.append(tuple_element)  # Append the tuple to the list
+    return result
+
+
+def create_spectra(wvl_range:tuple, band_width:int|float, disable_tqdm=False, save_spectra=False, **kwargs) -> tuple:
+    a = np.arange(wvl_range[0], wvl_range[1],band_width)
+    b = np.arange(wvl_range[0] + band_width, wvl_range[1] + band_width, band_width)
+    wvl_bands = create_list_of_tuples(a,b)
+    center_bands = np.mean(wvl_bands, axis=-1)
+    spectrum = np.array([])
+    for b in tqdm(wvl_bands, disable=disable_tqdm):
+        flux = projection_to_total_lum(b, **kwargs)
+        spectrum = np.append(spectrum, flux)
+    if save_spectra:
+        np.save(save_spectra, [center_bands, spectrum]) # type: ignore
+        return (center_bands, spectrum)
+    else:
+        return (center_bands, spectrum)
---
+++ src/Xray_winds/load_data.py
@@ -0,0 +1,121 @@
+from multiprocessing import Value
+import os
+from pyexpat import model
+from starwinds_readplt.dataset import Dataset
+import numpy as np
+import sys, os
+data_loc = os.environ['FMPdata']
+
+
+def find_file(name, path):
+    """This function makes use of glob.glob to find the file(s) named file, in directory path
+
+    Args:
+        name (str): _description_
+        path (str): _description_
+
+    Returns:
+        str: The location of the first file
+    """
+    import glob
+    files = glob.glob(data_loc + '/' + path + '/**/' + name, recursive=True)
+    print('{}'.format(f'Reading from file: {files[0]}'))
+    return files[0]
+
+def read_model(model_name:str):
+    try:
+        model_path = find_file('3d__var_3_n00060000.plt', model_name)
+    except IndexError:
+        print('3d__var_3_n00060000.plt not found, trying 3d__var_3_n00006000.plt')
+        model_path = find_file('3d__var_3_n00006000.plt', model_name)
+
+    ds = Dataset.from_file(model_path)
+    try:
+        params_file = open(os.path.join(data_loc, model_name + '/SCPARAM.in'), 'r')
+        model_params = params_file.read()
+        params_file.close()
+    except:
+        print("No SCPARAM.in found.")
+        model_params = None
+    try:
+        star_file = open(os.path.join(data_loc, model_name + '/STAR.in'), 'r')
+        param_lines = star_file.readlines()
+        star_params = {}
+        for n, line in enumerate(param_lines[-3:]):
+            split = line.split('  ')
+            value = split[0]
+            name = split[-1].split(' ')
+            if name[0].isalpha() == False:
+                name = split[-1].split(' ')[1]
+            else:
+                name = name[0]
+            star_params[str(name)] = float(value)
+    except:
+        print("No STAR.in found. Defaulting to solar parameters")
+        star_params = {'RadiusStar': 1, 'MassStar': 1, "RotationPeriodStar": 24.47}
+    return ds, model_params, star_params
+
+
+def import_data(name:str, interpolate='nearest', full_output=False, verbose=False):
+    assert type(full_output) == bool
+    """This function imports the data of the star given in the name parameter. It uses the files from the model to get the grid into units of cm such that it can be used with CGS
+
+
+    Args:
+        name (str): Name of the model 
+        interpolate (str, optional): _description_. Defaults to 'nearest'.
+        full_output (bool, optional): If True, also returns the data points used to create the interpolater, and returns the model_params
+
+    Returns:
+        _type_: _description_
+    """
+    import astropy.units as u
+    from scipy.interpolate import NearestNDInterpolator, LinearNDInterpolator
+    from scipy.spatial.qhull import QhullError
+    if verbose:
+        ds, model_params, star_params = read_model(name)
+    elif not verbose:
+        block_print()
+        ds, model_params, star_params = read_model(name)
+        resume_print()
+    variable_list = ds.variables
+    # Get a stack of points and the data in a numpy format and create an interpolator function
+    ds_points = np.stack([ds(name) for name in ds.variables[:3]], axis=-1)
+    radius_star = star_params['RadiusStar'] * u.R_sun
+    star_params['RadiusStar'] = radius_star.to(u.cm).value
+    ds_points *= radius_star.to(u.cm).value
+    ds_data = np.stack([ds(name) for name in ds.variables], axis=-1)
+
+    if not full_output:
+        if interpolate.lower() == 'nearest':
+            return NearestNDInterpolator(ds_points, ds_data), variable_list, star_params, None, None, None
+        elif interpolate.lower() == 'linear':
+            try:
+                return LinearNDInterpolator(ds_points, ds_data), variable_list, star_params, None, None, None
+            except QhullError:
+                print("QhullError: Linear interpolation failed, \
+                Using nearest interpolation") 
+                return NearestNDInterpolator(ds_points, ds_data), variable_list, star_params, None, None, None
+        else:
+            raise ValueError
+    else:
+        if interpolate.lower() == 'nearest':
+            return NearestNDInterpolator(ds_points, ds_data),  variable_list, star_params, model_params, ds_points, ds_data
+        elif interpolate.lower() == 'linear':
+            try:
+                return LinearNDInterpolator(ds_points, ds_data), variable_list, star_params, model_params, ds_points, ds_data
+            except QhullError:
+                print("QhullError: Linear interpolation failed, \
+                Using nearest interpolation") 
+                return NearestNDInterpolator(ds_points, ds_data),  variable_list, star_params, model_params, ds_points, ds_data
+        else:
+            raise ValueError
+        
+def block_print():
+    sys.stdout = open(os.devnull, 'w')
+
+def resume_print():
+    sys.stdout = sys.__stdout__
+
+if __name__ == "__main__":
+    _ = import_data('1x-PWAnd')
